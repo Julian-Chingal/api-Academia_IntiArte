19b0a3aa801fa4477fb53b09e26d4982584c3571
Julian-Chingal <mauriciochingal16@gmail.com>
2023-08-27 00:49:05 -0500

    commit

--- src/database.js
+++ src/database.js
@@ -1,4 +1,9 @@
 const {PrismaClient} = require('@prisma/client')
 const prisma = new PrismaClient()
 
+if (prisma) {
+    console.log("Conexion exitosa")
+}else{ 
+    console.log("Conexion Fallida")
+}
 module.exports = prisma
